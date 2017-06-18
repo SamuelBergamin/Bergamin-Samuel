9049849c233d74424aa316aec33024bb686cd683
SamuelBergamin <bergamin.samuel@gmail.com>
2017-06-18 15:14:08 +0200

    first commit

---
+++ bottoni.php
@@ -0,0 +1,67 @@
+<?php
+	$title = "alpi";
+	include_once 'header.php';
+?>
+<style>
+.button {
+  display: inline-block;
+  padding: 15px 25px;
+  font-size: 24px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  outline: none;
+  color: #fff;
+  background-color: #4CAF50;
+  border: none;
+  border-radius: 15px;
+  box-shadow: 0 9px #999;
+}
+
+.button:hover {background-color: #3e8e41}
+
+.button:active {
+  background-color: #3e8e41;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
+}
+</style>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button><button class="button">Clickami</button>
+<?php
+	include_once 'footer.php';
+?>
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+	$title = "alpi";
+	include_once 'header.php';
+?>
+	<img src="./images/gif_iniziale.gif">
+	<h1>Titolo</h1>
+	<h2>Titolo</h2>
+	<h3>Titolo</h3>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+			<h5>Prova bootstrap</h5>
+			<p>freferfrffreferferfijeriferiferiferfiefkfijerifjeri</p>	
+			<i>Corsivo</i>
+			</div>
+			<div class="col-sm-4">
+			<h5>Prova bootstrap</h5>
+			<p>freferfrffreferferfijeriferiferiferfiefkfijerifjerf</p>
+			<b>Grassetto</b>
+			</div>
+			<div class="col-sm-4">
+			<h5>Prova bootstrap</h5>
+			<p>freferfrffreferferfijeriferiferiferfiefkfijerifjerf</p>
+			<u>Sottolineato</u>
+			</div>
+		</div>
+	</div>
+	<marquee>Testo che scorre - Testo che scorre - Testo che scorre - Testo che scorre - Testo che scorre - Testo che scorre - Testo che scorre</marquee>
+	
+<?php
+	include_once 'footer.php';
+?>
